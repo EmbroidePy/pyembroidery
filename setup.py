8351f86dd3f6732af17446e83945614599962c31
tatarize <tatarize@gmail.com>
2018-11-03 20:14:00 -0700

    update setup 1.3.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyembroidery",
-    version="1.3.5",
+    version="1.3.6",
     author="Tatarize",
     author_email="tatarize@gmail.com",
     description="Embroidery IO library",
