213ecc2fc4d98d0e8cb9956f003d91f53b56548c
tatarize <tatarize@gmail.com>
2018-11-06 03:20:14 -0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyembroidery",
-    version="1.3.7",
+    version="1.3.8",
     author="Tatarize",
     author_email="tatarize@gmail.com",
     description="Embroidery IO library",
