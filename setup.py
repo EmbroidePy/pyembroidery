8be565e9e5bca5e034da483b81f402f13ab6469d
tatarize <tatarize@gmail.com>
2019-08-14 20:46:33 -0700

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyembroidery",
-    version="1.4.2",
+    version="1.4.3",
     author="Tatarize",
     author_email="tatarize@gmail.com",
     description="Embroidery IO library",
