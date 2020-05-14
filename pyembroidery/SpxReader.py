f7f20fdecb4871def5df6361873544c539b3e874
tatarize <tatarize@gmail.com>
2020-05-14 08:50:15 -0900

    Cursory SpxReader

--- pyembroidery/SpxReader.py
+++ pyembroidery/SpxReader.py
@@ -1,4 +1,4 @@
-from .ReadHelper import read_int_16le, read_int_8, signed16
+from .ReadHelper import read_int_16be, read_int_8, signed8, signed16
 
 
 def read(f, out, settings=None):
@@ -7,18 +7,11 @@ def read(f, out, settings=None):
         b = bytearray(f.read(2))
         if len(b) != 2:
             break
-        dx = signed16(read_int_16le(f))
-        dy = signed16(read_int_16le(f))
-        c = read_int_8(f)
+        dy = -signed16(read_int_16be(f))
+        dx = signed16(read_int_16be(f))
+        c = signed8(read_int_8(f))
+        dy -= c
         b = bytearray(f.read(2))
         if len(b) != 2:
             break
-        if c == 0x00:
-            out.stitch(dx, dy)
-            continue
-        if c == 0xA0:
-            out.move(dx, dy)
-            continue
-        if c == 0xF6:
-            out.move(dx, dy)
-            continue
+        out.stitch(dx, dy)
