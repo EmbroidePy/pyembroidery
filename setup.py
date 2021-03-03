03aeec29f975ba98e6ad25cfa046556a9d8e2826
tatarize <tatarize@gmail.com>
2021-03-02 16:01:24 -0800

    1.4.28

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyembroidery",
-    version="1.4.27",
+    version="1.4.28",
     author="Tatarize",
     author_email="tatarize@gmail.com",
     description="Embroidery IO library",
