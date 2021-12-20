6b66a19a2fc6773494de470c8551c64465f46bfc
Tatarize <tatarize@gmail.com>
2021-12-20 02:25:36 -0800

    Version Bump 1.4.32

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyembroidery",
-    version="1.4.31",
+    version="1.4.32",
     author="Tatarize",
     author_email="tatarize@gmail.com",
     description="Embroidery IO library",
@@ -13,13 +13,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/EmbroidePy/pyembroidery",
     packages=setuptools.find_packages(),
-    classifiers=(
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         'Topic :: Software Development :: Libraries :: Python Modules'
-    ),
+    ],
 )
