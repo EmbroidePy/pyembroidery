7ff2b30480f7877ef109da06a31cce22d163f212
Tatarize <tatarize@gmail.com>
2021-12-20 19:39:43 -0800

    Test for keys in `supported_formats`.

---
+++ test/test_catalog.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+import unittest
+from pattern_for_tests import *
+
+
+class TestDataCatalog(unittest.TestCase):
+
+    def test_catalog_files(self):
+        for f in EmbPattern.supported_formats():
+            self.assertIn("extensions", f)
+            self.assertIn("extension", f)
+            self.assertIn("description", f)
+            self.assertIn("category", f)
