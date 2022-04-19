766737422ddfe49cff5ad062a74827e1b19fd521
Tatarize <tatarize@gmail.com>
2022-04-19 00:51:06 -0700

    correct tupleness

--- test/test_generic.py
+++ test/test_generic.py
@@ -38,30 +38,28 @@ class TestConverts(unittest.TestCase):
         print("write generic: ", file1)
 
     def test_generic_write_gcode(self):
-        gcode_writer_dict = (
-            {
-                "scale": (-0.1, -0.1),
-                "pattern_start": "(STITCH_COUNT: {stitch_total})\n"
-                "(THREAD_COUNT: {color_change_count})\n"
-                "(EXTENTS_LEFT: {extents_left:.3f})\n"
-                "(EXTENTS_TOP: {extends_top:.3f})\n"
-                "(EXTENTS_RIGHT: {extends_right:.3f})\n"
-                "(EXTENTS_BOTTOM: {extends_bottom:.3f})\n"
-                "(EXTENTS_WIDTH: {extends_width:.3f})\n"
-                "(EXTENTS_HEIGHT: {extends_height:.3f})\n"
-                "(COMMAND_STITCH: {stitch_count})\n"
-                "(COMMAND_COLOR_CHANGE: {color_change_count})\n"
-                "(COMMAND_TRIM: {trim_count})\n"
-                "(COMMAND_STOP: {stop_count})\n"
-                "(COMMAND_END: {end_count})\n",
-                "metadata_entry": "({metadata_key}: {metadata_value})\n",
-                "thread_entry": "(Thread{thread_index}: {thread_color} {thread_description} {thread_brand} {thread_catalog_number})\n",
-                "stitch": "G00 X{x:.3f} Y{y:.3f}\nG00 Z{z:.1f}\n",
-                "color_change": "M00\n",
-                "stop": "M00\n",
-                "end": "M30\n",
-            },
-        )
+        gcode_writer_dict = {
+            "scale": (-0.1, -0.1),
+            "pattern_start": "(STITCH_COUNT: {stitch_total})\n"
+            "(THREAD_COUNT: {color_change_count})\n"
+            "(EXTENTS_LEFT: {extents_left:.3f})\n"
+            "(EXTENTS_TOP: {extends_top:.3f})\n"
+            "(EXTENTS_RIGHT: {extends_right:.3f})\n"
+            "(EXTENTS_BOTTOM: {extends_bottom:.3f})\n"
+            "(EXTENTS_WIDTH: {extends_width:.3f})\n"
+            "(EXTENTS_HEIGHT: {extends_height:.3f})\n"
+            "(COMMAND_STITCH: {stitch_count})\n"
+            "(COMMAND_COLOR_CHANGE: {color_change_count})\n"
+            "(COMMAND_TRIM: {trim_count})\n"
+            "(COMMAND_STOP: {stop_count})\n"
+            "(COMMAND_END: {end_count})\n",
+            "metadata_entry": "({metadata_key}: {metadata_value})\n",
+            "thread_entry": "(Thread{thread_index}: {thread_color} {thread_description} {thread_brand} {thread_catalog_number})\n",
+            "stitch": "G00 X{x:.3f} Y{y:.3f}\nG00 Z{z:.1f}\n",
+            "color_change": "M00\n",
+            "stop": "M00\n",
+            "end": "M30\n",
+        }
         file1 = "file-gcode.gcode"
         file2 = "file-generic.gcode"
         pattern = get_fractal_pattern()
